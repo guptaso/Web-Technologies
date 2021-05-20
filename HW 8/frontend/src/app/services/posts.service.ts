f48db4688749423500ac19972771535de3ee5293
guptaso <guptaso@oregonstate.edu>
2021-05-20 00:50:42 -0700

    updated endpoints

--- HW 8/frontend/src/app/services/posts.service.ts
+++ HW 8/frontend/src/app/services/posts.service.ts
@@ -9,88 +9,88 @@ export class PostsService {
   constructor(private httpClient: HttpClient) { }
 
   getCarouselItems() {
-    let url = 'http://localhost:8080/apis/posts/current';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/current';
     return this.httpClient.get(url)
   }
 
   getPopularMovies() {
-    let url = 'http://localhost:8080/apis/posts/popular/movie';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/popular/movie';
     return this.httpClient.get(url);
   }
 
   getTopRatedMovies() {
-    let url = 'http://localhost:8080/apis/posts/topRated/movie';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/topRated/movie';
     return this.httpClient.get(url);
   }
 
   getTrendingMovies() {
-    let url = 'http://localhost:8080/apis/posts/trending/movie';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/trending/movie';
     return this.httpClient.get(url);
   }
 
   getPopularTV() {
-    let url = 'http://localhost:8080/apis/posts/popular/tv';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/popular/tv';
     return this.httpClient.get(url);
   }
 
   getTopRatedTV() {
-    let url = 'http://localhost:8080/apis/posts/topRated/tv';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/topRated/tv';
     return this.httpClient.get(url);
   }
 
   getTrendingTV() {
-    let url = 'http://localhost:8080/apis/posts/trending/tv';
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/trending/tv';
     return this.httpClient.get(url);
   }
 
   multiSearch(query: string) {
-    let url = 'http://localhost:8080/apis/posts/multisearch/' + query;
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/multisearch/' + query;
     return this.httpClient.get(url);
   }
 
   getVideo(type: string, id: string){
-    let url = 'http://localhost:8080/apis/posts/video/'+ type + '/' + id;
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/video/'+ type + '/' + id;
     return this.httpClient.get(url);
   }
 
   getDetails(type: string, id: string ){
-    let url = 'http://localhost:8080/apis/posts/detail/' + type + '/' + id ;
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/detail/' + type + '/' + id ;
     return this.httpClient.get(url);
   }
 
   getRecommended(type: string, id: string ){
-    let url = 'http://localhost:8080/apis/posts/recommended/' + type + '/' + id;
+    let url = 'https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/recommended/' + type + '/' + id;
     return this.httpClient.get(url);
   }
 
   getSimilar(type: string, id: string) {
-    let url = "http://localhost:8080/apis/posts/similar/" + type + '/' + id;
+    let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/similar/" + type + '/' + id;
     return this.httpClient.get(url);
   }
 
   getCast(type: string, id: string) {
-    let url = "http://localhost:8080/apis/posts/cast/" + type + '/' + id;
+    let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/cast/" + type + '/' + id;
     return this.httpClient.get(url);
   }
 
   getReview(type: string, id: string){
-    let url = "http://localhost:8080/apis/posts/review/" + type + '/' + id;
+    let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/review/" + type + '/' + id;
     return this.httpClient.get(url);
   }
 
   getCastDetails(id: string){
-    let url = "http://localhost:8080/apis/posts/castDetails/" + id;
+    let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/castDetails/" + id;
     return this.httpClient.get(url);
   }
 
   getCastHandles(id: string){
-    let url = "http://localhost:8080/apis/posts/castExternalId/" + id;
+    let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/castExternalId/" + id;
     return this.httpClient.get(url);
   }
 
   getSearch(query: string){
     if (query.length > 0){
-      let url = "http://localhost:8080/apis/posts/multisearch/" + query;
+      let url = "https://cedar-heaven-313505.wl.r.appspot.com/apis/posts/multisearch/" + query;
 
       return this.httpClient.get<[any, string[]]>(url)
     } else {
